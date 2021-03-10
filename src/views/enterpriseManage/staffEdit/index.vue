7fc35a19e139e77a29ba3e1934547789bf5ab2ae
username@email.com <1260027439@qq.com>
2021-03-10 15:49:26 +0800

    完成添加成员功能。

--- src/views/enterpriseManage/staffEdit/index.vue
+++ src/views/enterpriseManage/staffEdit/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="app-container staffEdit">
+  <div class="app-container staffEdit" v-loading="loading" element-loading-text="添加中...">
     <div class="titleEdit">添加成员</div>
-    <el-form :model="ruleFormAdd" ref="ruleFormAdd" label-width="120px">
+    <el-form :model="ruleFormAdd" ref="ruleFormAdd" :rules="rulesAdd" label-width="120px">
       <el-form-item label="姓名" prop="staff.name" :rules="{ required: true, message: '姓名为必填项', trigger: 'blur' }">
         <el-input size="mini" style="width: 340px" v-model="ruleFormAdd.staff.name" auto-complete="off" placeholder="姓名" />
       </el-form-item>
-      <el-form-item label="手机" prop="staff.phone" required>
+      <el-form-item label="手机" prop="staff.phone">
         <el-row>
           <el-col :span="24">
             <el-input size="mini" style="width: 340px" v-model="ruleFormAdd.staff.phone" auto-complete="off" placeholder="手机号码" />
@@ -41,11 +41,11 @@
       </el-form-item>
 
       <el-form-item>
-        <div style="text-align: center; margin-right: 120px">
+        <div style="margin-left:250px;margin-top:20px">
             <router-link to="/enterpriseManage/addressBook">
                 <el-button size="mini">取 消</el-button>
             </router-link>
-          <el-button style="margin-left: 20px" type="primary" size="mini" @click="submitForm('ruleFormAdd', true)">保 存</el-button>
+          <el-button style="margin-left: 20px" type="primary" size="mini" @click="submitForm('ruleFormAdd')">保 存</el-button>
           <!-- <el-button style="margin-left: 20px" type="primary" size="mini" @click="submitForm('ruleFormAdd', false)">保存并继续添加</el-button> -->
         </div>
       </el-form-item>
@@ -54,12 +54,22 @@
 </template>
 
 <script>
+import { add_staff } from '@/api/enterpriseManage'
 import { mapGetters } from 'vuex'
 export default {
   name: "staffEdit",
   data() {
+    var contact_phoneCheck = (rule, value, callback) => {
+      var exe = /^1[3456789]{1}\d{9}$/
+      if (value === '') {
+        callback(new Error('手机号不能为空'));
+      } else if (!exe.exec(value)) {
+        callback(new Error('请正确输入手机号'));
+      } else {
+        callback();
+      }
+    };
     return {
-      rulesAdd: [],
       ruleFormAdd: {
         department_name: "",
         staff: {
@@ -72,8 +82,14 @@ export default {
           },
         },
       },
+      rulesAdd: {
+        'staff.phone': [
+          {validator: contact_phoneCheck,}
+        ],
+      },
       workpieces: [],
       checked: true,
+      loading: false
     };
   },
   computed: {
@@ -86,14 +102,56 @@ export default {
     if(!this.isEdit){
       this.workpiece_list.forEach(node => {
         this.workpieces.push(node.id)
-        this.ruleFormAdd.staff.config.workpieces.push(node)
       })
+      this.$set(this.ruleFormAdd.staff.config,'workpieces',this.workpiece_list)
     }else{
       console.log('编辑成员');
     }
   },
   methods: {
-      isGetAll(){}
+    isGetAll(){},
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.handleDiaSureAdd(formName)
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    async handleDiaSureAdd(formName,key){
+      this.loading = true;
+      let forData = this.ruleFormAdd
+
+      forData.staff.config.workpieces = []
+      this.workpiece_list.forEach(node => {
+        this.workpieces.forEach(id => {
+          if (id == node.id) {
+            forData.staff.config.workpieces.push(node)
+          }
+        })
+      })
+
+      let result = await add_staff({
+        access_token: this.token,
+        ...forData
+      });
+      if(result.code===0){
+        this.loading = false;
+        this.$notify({
+          type: 'success',
+          title: '成功',
+          message: '操作成功!'
+        });
+        this.resetForm(formName)
+      }else{
+        this.loading = false;
+      }
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
 };
 </script>
@@ -109,9 +167,6 @@ export default {
     margin-bottom: 30px;
     border-left: 4px solid #00a0e9;
     padding-left: 24px;
-  }
-  .el-form-item {
-      margin-bottom: 12px;
   }
 }
 </style>
