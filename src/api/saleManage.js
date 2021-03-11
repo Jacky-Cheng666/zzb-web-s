4ea0f4d57e0fce1fe98232315504136834fc1118
Leon <9994323@qq.com>
2021-03-11 09:39:53 +0800

    获取销售基本信息接口修改

--- src/api/saleManage.js
+++ src/api/saleManage.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getSaleBasicInfo(data) {
   return request({
-    url: '/scm/company/get_buy_basic_info',
+    url: '/som/orderCommon/get_sale_basic_info',
     method: 'post',
     data
   })
