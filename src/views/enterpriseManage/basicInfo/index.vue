63d64a5fee894299b0bbfebd95945bb96ee551bc
username@email.com <1260027439@qq.com>
2021-03-16 10:29:25 +0800

    调整按钮位置。

--- src/views/enterpriseManage/basicInfo/index.vue
+++ src/views/enterpriseManage/basicInfo/index.vue
@@ -4,12 +4,14 @@
       <div class="header">
         <div class="left">
           <span class="tipWord">账本信息</span>
+        </div>
+        <div>
           <el-input v-model="financialBookName" style="width:120px" size="mini" placeholder="输入账本名称"></el-input>
           <el-button @click="saveFinancialBook" size="mini" type="primary">保存账本名称</el-button>
+          <el-select style="width:140px;margin:0 10px" size="mini" @change="switchFinancialBook" v-model="CurrentFinancialBook" placeholder="请选择">
+            <el-option v-for="item in bookList" :key="item.financial_book_no" :label="item.financial_book_name" :value="item.financial_book_no" />
+          </el-select>
         </div>
-        <el-select style="width:102px;margin-right:10px" size="mini" @change="switchFinancialBook" v-model="CurrentFinancialBook" placeholder="请选择">
-          <el-option v-for="item in bookList" :key="item.financial_book_no" :label="item.financial_book_name" :value="item.financial_book_no" />
-        </el-select>
       </div>
       <div class="info">
         <div class="title">
